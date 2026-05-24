73f01b578d75de1ac8133deec500d46de2415d11
Himanshu-07-Sunariya <himanshu.sunariya24@gmail.com>
2026-05-24 23:54:28 +0530

    Counter App launched

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Counter = () => {
-  return (
-    <div>
-      hiiii
-    </div>
-  )
+    const [count,setCount] = useState(0);
+
+    function increment(){
+        setCount(count+1);
+    }
+
+    function decrement(){
+        setCount(count-1);
+    }
+
+    function reset(){
+        setCount(0);
+    }
+    
+    return (
+        <div>
+            <h1>Counter App</h1>
+
+            <h2>{count}</h2>
+
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+
 }
 
 export default Counter
